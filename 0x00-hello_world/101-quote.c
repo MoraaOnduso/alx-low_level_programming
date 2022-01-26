6e4541472c4c1a444bc744abe5553e5fa24b4164
MoraaOnduso <mary.ondusoo@gmail.com>
2022-01-26 02:29:05 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: prints strimg to the stderr
+ * Description: prints string to the stderr
  * Return: 1
  */
 
 int main(void)
 {
-fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char *s;
+s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, s, 59);
 return (1);
 }
+
